7eaa15ad3d7e02e1dc6587f62e4c1f55a87c5e79
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-19 15:06:16 +0100

    pwa added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 // This is to show Header on every pages!
 import Header from "./header";
@@ -6,6 +7,32 @@ import Header from "./header";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Next.js PWA Example</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/icon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/icon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
